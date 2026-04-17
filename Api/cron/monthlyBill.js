ca0effdf84459a808e8f6267962cc96ae85ae810
Bidyanshu Kumar <kumarbidyanshu66@gmail.com>
2026-04-17 23:40:07 +0530

    Added

--- Api/cron/monthlyBill.js
+++ Api/cron/monthlyBill.js
@@ -12,33 +12,33 @@ cron.schedule("59 23 * * *", async () => {
     tomorrow.setDate(today.getDate() + 1);
 
 
-    //   if (today.getMonth() !== tomorrow.getMonth()) {
+    if (today.getMonth() !== tomorrow.getMonth()) {
 
-    const month = today.toISOString().slice(0, 7);
-    const users = await User.find();
+        const month = today.toISOString().slice(0, 7);
+        const users = await User.find();
 
-    for (let user of users) {
-        const bill = await messBill.findOne({
-            user: user._id,
-            month,
-        });
+        for (let user of users) {
+            const bill = await messBill.findOne({
+                user: user._id,
+                month,
+            });
 
-        if (!bill) continue;
+            if (!bill) continue;
 
-        const pdfBuffer = await generateBillPDF(bill, user);
+            const pdfBuffer = await generateBillPDF(bill, user);
 
-        await sendEmail(
-            user.email,
-            "Your Monthly Mess Bill",
-            `<p>Hello ${user.name},<br/>Please find your bill attached.</p>`,
-            [
-                {
-                    filename: `MessBill-${month}.pdf`,
-                    content: pdfBuffer,
-                },
-            ]
-        );
-        // }
+            await sendEmail(
+                user.email,
+                "Your Monthly Mess Bill",
+                `<p>Hello ${user.name},<br/>Please find your bill attached.</p>`,
+                [
+                    {
+                        filename: `MessBill-${month}.pdf`,
+                        content: pdfBuffer,
+                    },
+                ]
+            );
+        }
 
         console.log("Monthly emails sent ");
     }
