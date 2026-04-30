b191c250a9f2c24214b5cca5150a45848d02138e
Bidyanshu Kumar <kumarbidyanshu66@gmail.com>
2026-04-30 10:42:11 +0530

    updated ipv4 access

--- Api/config/sendEmail.js
+++ Api/config/sendEmail.js
@@ -4,6 +4,8 @@ const sendEmail = async(email,subject,message)=>{
         host:'smtp.gmail.com',
         port:587,
         secure:false,
+        family: 4,  // it forces IPV4 
+        
         auth:{
             user:process.env.EMAIL_USER,
             pass:process.env.EMAIL_PASS
